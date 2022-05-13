63a1719acf9095c53c25d751c295913e97f37370
Mostafa ELsisy <Mostafa Elsisy>
2022-05-13 03:40:05 +0200

    Home from elsisy

---
+++ lib/elsisy/bookitem.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../core/models/book_model.dart';
+
+class BookItem extends StatelessWidget {
+  const BookItem({Key? key, required this.bookModel}) : super(key: key);
+  final BookModel bookModel;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 3,
+      width: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              height: 160,
+              width: 100,
+              child: Image(
+                image:
+                AssetImage('${bookModel.picture}'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            '${bookModel.nameEn}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+            maxLines: 3,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            '${bookModel.authorName}',
+            style: TextStyle(
+              color: Colors.grey[500],
+              fontStyle: FontStyle.italic,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
