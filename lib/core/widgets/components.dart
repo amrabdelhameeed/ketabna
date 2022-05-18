dc3b87b03e7ad413f2595435c980da04a5c16c46
BALY <baly12121999@gmail.com>
2022-05-18 09:42:24 +0200

    Merge remote-tracking branch 'origin/master'

--- lib/core/widgets/components.dart
+++ lib/core/widgets/components.dart
@@ -59,3 +59,12 @@ void navigateAndFinish (context , widget)
       (Route<dynamic> route) => false,
 
 );
+
+// Write be BALY
+void navigateTo (context , widget)
+=> Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => widget,
+  ),
+);
--- lib/features/authentication/sign_up/signup_screen.dart
+++ lib/features/authentication/sign_up/signup_screen.dart
@@ -26,6 +26,12 @@ class SignupPage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0.0,
+          // Updated by Baly
+          iconTheme: const IconThemeData(
+            color: AppColors.secondaryColor,
+            size: 32,
+          ),
+
           // leading: IconButton(
           //   padding: EdgeInsets.only(top: 20.0),
           //   onPressed: () {
@@ -41,6 +47,7 @@ class SignupPage extends StatelessWidget {
         ),
         body: SafeArea(
           child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Form(
@@ -109,38 +116,77 @@ class SignupPage extends StatelessWidget {
                         },
                         builder: (context, state) {
                           var cubit = AuthCubit.get(context);
-                          return DefaultFormButton(
-                            text: 'Sign Up',
-                            onPressed: () async {
-                              if (formKey.currentState!.validate()) {
-                                await cubit.signUpWithEmailAndPassword(
-                                  isWhatsapp: value,
-                                  interstsModel: InterstsModel(
-                                    biography: true,
-                                    children: true,
-                                    fantasy: true,
-                                    graphicNovels: true,
-                                    history: true,
-                                    horror: true,
-                                    romance: true,
-                                    scienceFiction: true,
+                          return Column(
+
+                            children: [
+                              DefaultFormButton(
+                                text: 'Sign Up',
+                                fontSize: 20,
+
+
+                                fillColor: AppColors.secondaryColor,
+                                onPressed: () async {
+                                  if (formKey.currentState!.validate()) {
+                                    await cubit.signUpWithEmailAndPassword(
+                                      isWhatsapp: value,
+                                      interstsModel: InterstsModel(
+                                        biography: true,
+                                        children: true,
+                                        fantasy: true,
+                                        graphicNovels: true,
+                                        history: true,
+                                        horror: true,
+                                        romance: true,
+                                        scienceFiction: true,
+                                      ),
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                      name: _nameController.text,
+                                      phone: _mobileController.text,
+                                    );
+                                  }
+                                },
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '  Do you have account ? ',
+                                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                                      fontSize: 15,
+                                    ),
+
                                   ),
-                                  email: _emailController.text,
-                                  password: _passwordController.text,
-                                  name: _nameController.text,
-                                  phone: _mobileController.text,
-                                );
-                              }
-                            },
+
+                                  TextButton(
+                                    onPressed: (){
+                                      Navigator.pushReplacementNamed(context, loginScreen);
+
+                                    },
+                                    child: Text(
+                                      'Sign In ',
+                                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                                        fontSize: 20,
+                                        color: AppColors.secondaryColor,
+                                      ),
+
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           );
                         },
                       ),
-                      DefaultFormButton(
-                        text: 'Sign In',
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, loginScreen);
-                        },
-                      )
+
+                      // DefaultFormButton(
+                      //   text: 'Sign In',
+                      //   onPressed: () {
+                      //     Navigator.pushReplacementNamed(context, loginScreen);
+                      //   },
+                      // )
+                      
+
                     ],
                   )),
             ),
