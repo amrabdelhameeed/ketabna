38e3f24f407fbd724f421850d74b7d9ef61f6b7a
BALY <baly12121999@gmail.com>
2022-06-03 15:22:58 +0200

    Merge branch 'amrabdelhameeed:master' into master
    BottomNavBar Screen

---
+++ lib/screens/bottom_navigation.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// void main() => runApp(MaterialApp(home: BottomNavBar()));
+// blue navigation bar  Write by baly
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _page = 0;
+   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 60.0,
+          items: const <Widget>  [
+            Icon(Icons.add, size: 30 ,color: Colors.white),
+            Icon(Icons.list, size: 30,color: Colors.white),
+            Icon(Icons.compare_arrows, size: 30,color: Colors.white),
+            Icon(Icons.call_split, size: 30,color: Colors.white),
+            Icon(Icons.perm_identity, size: 30,color: Colors.white),
+          ],
+          color: Colors.orangeAccent,
+          // buttonBackgroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(_page.toString(), textScaleFactor: 10.0),
+                ElevatedButton(
+                  child: Text('Go To Page of index 1'),
+                  onPressed: () {
+                    final CurvedNavigationBarState? navBarState =
+                        _bottomNavigationKey.currentState;
+                    navBarState?.setPage(1);
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
