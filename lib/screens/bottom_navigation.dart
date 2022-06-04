1c6a34d16e2927e75be292074514e8038b8f3daf
BALY <baly12121999@gmail.com>
2022-06-04 17:58:43 +0200

    Merge branch 'amrabdelhameeed:master' into master
    BottomNavBar Screen

--- lib/screens/bottom_navigation.dart
+++ lib/screens/bottom_navigation.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-
 // void main() => runApp(MaterialApp(home: BottomNavBar()));
 // blue navigation bar  Write by baly
 class BottomNavBar extends StatefulWidget {
@@ -11,7 +10,16 @@ class BottomNavBar extends StatefulWidget {
 
 class _BottomNavBarState extends State<BottomNavBar> {
   int _page = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  // new
+  List<Widget> screenWidget= [
+    // write screen widget
+    // 1 Home Screen
+    // 2 Search Screen
+    // 3 Add Screen
+    // 4 chat Screen
+    // 5 Profile Screen
+  ];
+   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -20,25 +28,28 @@ class _BottomNavBarState extends State<BottomNavBar> {
           key: _bottomNavigationKey,
           index: 0,
           height: 60.0,
-          items: const <Widget>[
-            Icon(Icons.add, size: 30, color: Colors.white),
-            Icon(Icons.list, size: 30, color: Colors.white),
-            Icon(Icons.compare_arrows, size: 30, color: Colors.white),
-            Icon(Icons.call_split, size: 30, color: Colors.white),
-            Icon(Icons.perm_identity, size: 30, color: Colors.white),
+          items: const <Widget>  [
+            Icon(Icons.home_outlined, size: 30 ,color: Colors.white),
+            Icon(Icons.search_outlined, size: 30,color: Colors.white),
+            Icon(Icons.add, size: 30,color: Colors.white),
+            Icon(Icons.message, size: 30,color: Colors.white),
+            Icon(Icons.perm_identity, size: 30,color: Colors.white),
           ],
           color: Colors.orangeAccent,
           // buttonBackgroundColor: Colors.black,
           backgroundColor: Colors.white,
           animationCurve: Curves.easeInOut,
-          animationDuration: const Duration(milliseconds: 600),
+          animationDuration: Duration(milliseconds: 600),
           onTap: (index) {
             setState(() {
               _page = index;
             });
           },
           letIndexChange: (index) => true,
+
         ),
+        // new
+        // body : screenWidget[_page]
         body: Container(
           color: Colors.white,
           child: Center(
@@ -47,7 +58,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
               children: <Widget>[
                 Text(_page.toString(), textScaleFactor: 10.0),
                 ElevatedButton(
-                  child: const Text('Go To Page of index 1'),
+                  child: Text('Go To Page of index 1'),
                   onPressed: () {
                     final CurvedNavigationBarState? navBarState =
                         _bottomNavigationKey.currentState;
