b66ff610c5fb545ff0a5f3752d4e08764fb46e06
Mostafa ELsisy <Mostafa Elsisy>
2022-06-06 00:39:38 +0200

    تغير الgenres مكان الnew releases

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -370,7 +370,85 @@ class HomeScreen extends StatelessWidget {
                               ),
                             ]),
 
-                        /// Header ==> Recommended
+
+                        /// Category
+                        defaultHeader(
+                          text: 'Genres',
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CarouselSlider.builder(
+                            itemCount: category.length,
+                            itemBuilder: (context, index, realIndex) => InkWell(
+                              borderRadius: BorderRadius.circular(20),
+                              onTap: () {
+                                // Navigator.pushNamed(context, categoryScreen);
+                              },
+                              child: Stack(
+                                alignment: AlignmentDirectional.center,
+                                children: [
+                                  Container(
+                                    width: 180,
+                                    decoration: BoxDecoration(
+                                        color: colors[index],
+                                        borderRadius:
+                                        BorderRadius.circular(20)),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        category[index].categoryName,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                        textAlign: TextAlign.center,
+                                        maxLines: 3,
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.black
+                                                    .withOpacity(0.5),
+                                                blurStyle: BlurStyle.normal,
+                                                blurRadius: 10,
+                                                offset: const Offset(0, 10))
+                                          ],
+                                        ),
+                                        height: MediaQuery.of(context)
+                                            .size
+                                            .height /
+                                            5,
+                                        width: 100,
+                                        child: Image(
+                                          image: AssetImage(
+                                              category[index].imagePath),
+                                          fit: BoxFit.fill,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            options: CarouselOptions(
+                              height: MediaQuery.of(context).size.height / 3,
+                              viewportFraction: 0.6,
+                              autoPlay: true,
+                              autoPlayAnimationDuration:
+                              const Duration(milliseconds: 600),
+                            )),
+                        const SizedBox(
+                          height: 10,
+                        ),
+
+                        /// Header ==> new Releases
                         defaultHeader(
                           text: 'New Releases',
                         ),
@@ -383,82 +461,6 @@ class HomeScreen extends StatelessWidget {
                           height: 10,
                         ),
 
-                        /// Category
-                        defaultHeader(
-                          text: 'Genres',
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        CarouselSlider.builder(
-                            itemCount: category.length,
-                            itemBuilder: (context, index, realIndex) => InkWell(
-                                  borderRadius: BorderRadius.circular(20),
-                                  onTap: () {
-                                    // Navigator.pushNamed(context, categoryScreen);
-                                  },
-                                  child: Stack(
-                                    alignment: AlignmentDirectional.center,
-                                    children: [
-                                      Container(
-                                        width: 180,
-                                        decoration: BoxDecoration(
-                                            color: colors[index],
-                                            borderRadius:
-                                                BorderRadius.circular(20)),
-                                      ),
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            category[index].categoryName,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.white),
-                                            textAlign: TextAlign.center,
-                                            maxLines: 3,
-                                          ),
-                                          const SizedBox(
-                                            height: 10,
-                                          ),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              boxShadow: [
-                                                BoxShadow(
-                                                    color: Colors.black
-                                                        .withOpacity(0.5),
-                                                    blurStyle: BlurStyle.normal,
-                                                    blurRadius: 10,
-                                                    offset: const Offset(0, 10))
-                                              ],
-                                            ),
-                                            height: MediaQuery.of(context)
-                                                    .size
-                                                    .height /
-                                                5,
-                                            width: 100,
-                                            child: Image(
-                                              image: AssetImage(
-                                                  category[index].imagePath),
-                                              fit: BoxFit.fill,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                            options: CarouselOptions(
-                              height: MediaQuery.of(context).size.height / 3,
-                              viewportFraction: 0.6,
-                              autoPlay: true,
-                              autoPlayAnimationDuration:
-                                  const Duration(milliseconds: 600),
-                            )),
-                        const SizedBox(
-                          height: 10,
-                        ),
 
                         BlocListener<AuthCubit, AuthState>(
                           listener: (context, state) {
