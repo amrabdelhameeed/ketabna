d8e483fb564bc1c0c222d115476d2372a3090552
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2022-06-06 00:06:24 +0200

    add chat

--- lib/screens/visitor_screen.dart
+++ lib/screens/visitor_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ketabna/bloc/cubit/auth_cubit.dart';
@@ -6,15 +8,112 @@ import 'package:ketabna/core/widgets/default_form_button.dart';
 import 'package:ketabna/features/home/widgets/bookitem.dart';
 
 import '../core/utils/app_colors.dart';
+import '../core/utils/shared_pref_helper.dart';
+import '../core/widgets/components.dart';
+import '../features/chat/chat_screen.dart';
+
+String conversationDocId = '';
+final _firestore = FirebaseFirestore.instance;
+final String? myUid = FirebaseAuth.instance.currentUser?.uid.toString();
+final myName = SharedPrefHelper.getStr(key: 'userName');
 
 class VisitorScreen extends StatefulWidget {
   const VisitorScreen({Key? key, required this.userModel}) : super(key: key);
   final UserModel userModel;
+
   @override
   State<VisitorScreen> createState() => _VisitorScreenState();
 }
 
 class _VisitorScreenState extends State<VisitorScreen> {
+  Future<String> checkForOldConversation(
+      {bookOwnerUid, bookOwnerName, myId}) async {
+    String ids;
+    bool containUid_1 = false;
+    bool containUid_2 = false;
+    bool oldchat = false;
+    try {
+      await FirebaseFirestore.instance
+          .collection("chats")
+          .get()
+          .then((value) => {
+                value.docs.forEach((element) async {
+                  ids = element.data()['ids'].toString();
+                  print('ids :' + ids);
+                  final numberOfMessagesOfChat =
+                      element.data()['messages'].length;
+                  containUid_1 = ids.contains(bookOwnerUid);
+                  containUid_2 = ids.contains(myId);
+
+                  if (containUid_1 && containUid_2) {
+                    oldchat = true;
+                    print('Start !!');
+                    int bookOwnerReadMessages =
+                        element.data()['readed_messages'][bookOwnerUid];
+                    conversationDocId = element.id;
+                    await _firestore
+                        .collection('chats')
+                        .doc(conversationDocId)
+                        .update({
+                      'readed_messages': {
+                        '$myId': numberOfMessagesOfChat,
+                        '$bookOwnerUid': bookOwnerReadMessages,
+                      },
+                    }).then((value) => {
+                              navigateTo(
+                                  context: context,
+                                  widget: ChatScreen(
+                                    ownerName: bookOwnerName,
+                                    ownerUid: bookOwnerUid,
+                                    conversationDocId: conversationDocId,
+                                  )),
+                              // navigateTo(context: context,widget: ),
+                            });
+                  }
+                })
+              })
+          .then((value) => {
+                if (!oldchat)
+                  {
+                    setNewConversation(
+                        bookOwnerName: bookOwnerName,
+                        bookOwnerUid: bookOwnerUid)
+                  }
+              });
+    } catch (error) {
+      print(error.toString());
+    }
+
+    return conversationDocId;
+  }
+
+  Future setNewConversation({bookOwnerUid, bookOwnerName}) async {
+    await _firestore.collection("chats").add({
+      'names': [bookOwnerName, myName],
+      'ids': [
+        bookOwnerUid,
+        myUid,
+      ],
+      'messages': [],
+      'lastMessageTime': '',
+      'readed_messages': {
+        '$bookOwnerUid': 0,
+        '$myUid': 0,
+      },
+    }).then((documentSnapshot) => {
+          navigateTo(
+            context: context,
+            widget: ChatScreen(
+              ownerName: bookOwnerName,
+              ownerUid: bookOwnerUid,
+              conversationDocId: conversationDocId,
+            ),
+          ),
+          print("Added Data with ID: ${documentSnapshot.id}"),
+          conversationDocId = documentSnapshot.id,
+        });
+  }
+
   List<Map<String, Object>> itemBook = [
     {
       'title': 'Book',
@@ -108,6 +207,35 @@ class _VisitorScreenState extends State<VisitorScreen> {
                         width: 30,
                       ),
                       DefaultFormButton(
+                        onPressed: () async {
+                          String ownerUid = widget.userModel.userUid.toString();
+                          String ownerName = '';
+                          await FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(ownerUid)
+                              .get()
+                              .then(
+                                (value) => {
+                                  print('ownerName :' + value['name']),
+                                  ownerName = value['name'],
+                                },
+                              )
+                              .then((value) => {
+                                    checkForOldConversation(
+                                            bookOwnerUid: ownerUid,
+                                            myId: myUid,
+                                            bookOwnerName: ownerName)
+                                        .then((value) => {
+                                              // navigateTo(
+                                              //     context: context,
+                                              //     widget: ChatScreen(
+                                              //       ownerName: ownerName,
+                                              //       ownerUid: ownerUid,
+                                              //       conversationDocId: value,
+                                              //     )),
+                                            })
+                                  });
+                        },
                         text: 'Chat',
                         width: 120,
                         height: 35,
@@ -155,41 +283,41 @@ class _VisitorScreenState extends State<VisitorScreen> {
         listener: (context, state) {});
   }
 
-  // Widget buildItem() {
-  //   return Column(
-  //     // crossAxisAlignment: CrossAxisAlignment.start,
-  //     children: [
-  //       Expanded(
-  //         child: ClipRRect(
-  //           borderRadius: BorderRadius.circular(10),
-  //           child: const Image(
-  //             image: AssetImage('assets/image/ph.jpg'),
-  //           ),
-  //         ),
-  //       ),
-  //       const SizedBox(
-  //         height: 5,
-  //       ),
-  //       const Text(
-  //         'name',
-  //         style: TextStyle(
-  //           fontWeight: FontWeight.bold,
-  //         ),
-  //         maxLines: 3,
-  //       ),
-  //       const SizedBox(
-  //         height: 5,
-  //       ),
-  //       Text(
-  //         'dis',
-  //         style: TextStyle(
-  //           color: Colors.grey[500],
-  //           fontStyle: FontStyle.italic,
-  //         ),
-  //         maxLines: 2,
-  //         overflow: TextOverflow.ellipsis,
-  //       ),
-  //     ],
-  //   );
-  // }
+// Widget buildItem() {
+//   return Column(
+//     // crossAxisAlignment: CrossAxisAlignment.start,
+//     children: [
+//       Expanded(
+//         child: ClipRRect(
+//           borderRadius: BorderRadius.circular(10),
+//           child: const Image(
+//             image: AssetImage('assets/image/ph.jpg'),
+//           ),
+//         ),
+//       ),
+//       const SizedBox(
+//         height: 5,
+//       ),
+//       const Text(
+//         'name',
+//         style: TextStyle(
+//           fontWeight: FontWeight.bold,
+//         ),
+//         maxLines: 3,
+//       ),
+//       const SizedBox(
+//         height: 5,
+//       ),
+//       Text(
+//         'dis',
+//         style: TextStyle(
+//           color: Colors.grey[500],
+//           fontStyle: FontStyle.italic,
+//         ),
+//         maxLines: 2,
+//         overflow: TextOverflow.ellipsis,
+//       ),
+//     ],
+//   );
+// }
 }
